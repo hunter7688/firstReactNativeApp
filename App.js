27e552ab9992815d54d1b7e637ad2acff380990b
ROBIN HUNTER <hhhhhunter@ROBINHUTERsiMac.attlocal.net>
2018-09-19 10:22:50 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Somebody told me that</Text>
+        <Text>You're so stupid. I didn't believe them.</Text>
+        <Text>But now I believe them.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
